c57b71f581c59bf1e4f5a53136fe8298a6072664
BouncyHunter <43461964+BouncyHunter@users.noreply.github.com>
2018-09-26 13:37:39 +0800

    Add files via upload

--- Dawnfall/src/dawnfall/ProjectileFactory.java
+++ Dawnfall/src/dawnfall/ProjectileFactory.java
@@ -4,13 +4,21 @@ import javax.swing.JLabel;
 
 public class ProjectileFactory {
 	public static final int FIREBALL = 1;
-	public JLabel create(int id) {
-	JLabel label = new JLabel();
+	public static Projectile create(int id) {
+	Projectile p = null;
 		switch(id)
 		{
 		case FIREBALL:
-			label.setBounds((int) ((Gameboard.FRAME_X - 20)* Math.random()), 0, 20, 20);
+			p = new Projectile() {
+				public void action() {
+					this.setBounds(this.getX() + getVector_x(),this.getY() + getVector_y(),getWidth(),getHeight());
+				}
+			};
+			p.setVector_x(0);
+			p.setVector_y(5);
+			p.setBounds((int) ((Gameboard.FRAME_X - 20)* Math.random()), 0, 20, 20);
 		}
+		return p;
 	}
 
 }
