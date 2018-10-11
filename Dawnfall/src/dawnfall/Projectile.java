d06488f8cb838b25a3c2970556ff91574a9a10e0
BouncyHunter <43461964+BouncyHunter@users.noreply.github.com>
2018-10-11 13:28:27 +0800

    Add files via upload

--- Dawnfall/src/dawnfall/Projectile.java
+++ Dawnfall/src/dawnfall/Projectile.java
@@ -1,12 +1,33 @@
-
-
-
-
-
 package dawnfall;
 
-public interface Projectile {
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+
+public class Projectile extends JLabel implements Trigger{
 	
-	public boolean action();
+	Gameboard active_pane;
+	
+	public Projectile(Gameboard p)
+	{
+		active_pane = p;
+		
+		this.setBounds((int)(Math.random() * Gameboard.FRAME_X), (int)(Math.random() * Gameboard.FRAME_Y), 20, 20);
+		this.setIcon(new ImageIcon("./resource/Projectile.png"));
+		p.add(this);
+		p.addTrigger(this);
+	}
+	
+	@Override
+	public void actionPerformed() {
+		// TODO Auto-generated method stub
+		System.out.println("Howdy. Howdy.");
+	}
+
+	@Override
+	public Gameboard getActivePane() {
+		// TODO Auto-generated method stub
+		return active_pane;
+	}
 
 }
