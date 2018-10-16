afa9780e7f1dce64515707955f936343116795da
BouncyHunter <43461964+BouncyHunter@users.noreply.github.com>
2018-10-16 13:29:52 +0800

    Add files via upload

--- Dawnfall/src/dawnfall/Gameboard.java
+++ Dawnfall/src/dawnfall/Gameboard.java
@@ -1,8 +1,10 @@
 package dawnfall;
 
 import java.awt.Color;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
@@ -10,19 +12,31 @@ import javax.swing.JProgressBar;
 import javax.swing.Timer;
 
 public class Gameboard extends JLayeredPane{
-	private Npc[] npcs;
+	Timer[] text_timer;
+	//private Npc[] npcs;
 	JProgressBar health_bar;
 	public static final int FRAME_X = 600,FRAME_Y = 480,DEADLINE_WIDTH = 400,DEADLINE_HEIGHT = 30;
+	//public static final int TEXT_STACK = 5;
+	//int timer_pointer;
 	Timer main_timer;
 	int health = 100;
+	
+	ArrayList<Trigger> trigger_list = new ArrayList<Trigger>();
+    ArrayList<Trigger> buffer_trigger = new ArrayList<Trigger>();
+    ArrayList<Trigger> exile_trigger = new ArrayList<Trigger>();
 	
 	public Gameboard() {
 		setBounds(0,0,FRAME_X,FRAME_Y);
 		health_bar = new JProgressBar(0,health);
 		health_bar.setBounds((FRAME_X - DEADLINE_WIDTH) / 2, FRAME_Y - DEADLINE_HEIGHT, DEADLINE_WIDTH, DEADLINE_HEIGHT);
 		health_bar.setBackground(Color.RED);
-		main_timer = new Timer(20,new GBListener());
+		main_timer = new Timer(100,new GBListener());
 	}
+	
+	/*public void intitalizeTimer() {
+		text_timer = new Timer[TEXT_STACK];
+		timer_pointer = 0;
+	}*/
 	
 	public void start()
 	{
@@ -30,16 +44,27 @@ public class Gameboard extends JLayeredPane{
 	}
 	
 
-	public Npc[] getNpcs() {
+	/*public Npc[] getNpcs() {
 		return npcs;
 	}
 
 	public void setNpcs(Npc[] npcs) {
 		this.npcs = npcs;
-	}
+	}*/
 	
 	public void victory() {
 		
+	}
+	
+	public void addTrigger(Trigger t) {
+		buffer_trigger.add(t);
+	}
+	
+	public void removeTrigger(Trigger t) {
+		if(trigger_list.contains(t) && !exile_trigger.contains(t))
+		{
+			exile_trigger.add(t);
+		}
 	}
 	
 	public boolean takeDamage(int a) {
@@ -49,37 +74,19 @@ public class Gameboard extends JLayeredPane{
 		else return true;
 	}
 	
-	public void staticLabel(String text, int duration) {
-		JLabel tempo_label = new JLabel(text);
-		Timer tempo_timer = new Timer(duration,new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				remove(tempo_label);
-				
-			}
-			
-		});
-	}
-	
 	class GBListener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(npcs.length == 0)
-			{
-				main_timer.stop();
-				victory();
-				return;
+			for(Trigger t : trigger_list) {
+				t.actionPerformed(0);
 			}
-			for(Npc n : npcs) {
-				n.setCooldown(n.getCooldown() + n.getCooldownBoost());
-				if(n.getCooldown() >= n.getMaxCooldown())
-				{
-					n.setCooldown(0);
-					n.action();
-				}
+			for(Trigger t : buffer_trigger) {
+				trigger_list.add(t);
 			}
+			trigger_list.removeAll(exile_trigger);
+			exile_trigger.removeAll(exile_trigger);
+			buffer_trigger.removeAll(buffer_trigger);
 		}
 		
 	}
--- Dawnfall/src/dawnfall/Projectile.java
+++ Dawnfall/src/dawnfall/Projectile.java
@@ -1,45 +1,33 @@
 package dawnfall;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 
-public class Projectile extends JLabel{
-	private int vector_x,vector_y;
-	private int speed,duration;
-	private Gameboard active_pane;
-	public void action()
+public class Projectile extends JLabel implements Trigger{
+	
+	Gameboard active_pane;
+	
+	public Projectile(Gameboard p)
 	{
+		active_pane = p;
 		
+		this.setBounds((int)(Math.random() * Gameboard.FRAME_X), (int)(Math.random() * Gameboard.FRAME_Y), 20, 20);
+		this.setIcon(new ImageIcon("./resource/Projectile.png"));
+		p.add(this);
+		p.addTrigger(this);
 	}
-	public int getVector_x() {
-		return vector_x;
+	
+	@Override
+	public void actionPerformed(int index) {
+		// TODO Auto-generated method stub
+		System.out.println("Howdy. Howdy.");
 	}
-	public void setVector_x(int vector_x) {
-		this.vector_x = vector_x;
-	}
-	public int getVector_y() {
-		return vector_y;
-	}
-	public void setVector_y(int vector_y) {
-		this.vector_y = vector_y;
-	}
-	public int getSpeed() {
-		return speed;
-	}
-	public void setSpeed(int speed) {
-		this.speed = speed;
-	}
-	public int getDuration() {
-		return duration;
-	}
-	public void setDuration(int duration) {
-		this.duration = duration;
-	}
+
+	@Override
 	public Gameboard getActivePane() {
+		// TODO Auto-generated method stub
 		return active_pane;
-	}
-	public void setActivePane(Gameboard active_pane) {
-		this.active_pane = active_pane;
 	}
 
 }
